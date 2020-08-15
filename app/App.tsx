362c5f04af2473c35100ab36bbbfc9f6706b7cf6
Henry Moule <henry@care-planner.co.uk>
2020-08-16 00:42:01 +0100

    switched to drawer navigator

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DrawerActions} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from './Home';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
@@ -9,8 +11,13 @@ import {colors} from './constants';
 import Video from './Video';
 import Documents from './Documents';
 import Audio from './Audio';
+import Injil from './Injil';
+import {TouchableOpacity} from 'react-native';
 
 const Tab = createBottomTabNavigator();
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const theme = {
   ...DefaultTheme,
@@ -21,62 +28,43 @@ const theme = {
   },
 };
 
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Takat" component={Home} />
+      <Drawer.Screen name="Injil" component={Injil} />
+      <Drawer.Screen name="sawl-agh-d s-watsapp" component={WhatsApp} />
+    </Drawer.Navigator>
+  );
+}
+
 const App = () => {
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator
-          tabBarOptions={{
-            activeTintColor: colors.buttonHover,
-            inactiveTintColor: colors.button,
-            showLabel: false,
-          }}>
-          <Tab.Screen
-            options={() => ({
-              tabBarIcon: ({focused, color, size}) => {
-                return <Icon name="home" size={size} color={color} />;
-              },
-            })}
-            name="Home"
-            component={Home}
-          />
-          <Tab.Screen
-            options={(route) => ({
-              tabBarIcon: ({focused, color, size}) => (
-                <Icon name="headphones" size={size} color={color} />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Drawer"
+            component={MyDrawer}
+            options={({navigation}) => ({
+              headerStyle: {backgroundColor: colors.button},
+              headerTitle: '',
+              headerLeft: () => (
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.dispatch(DrawerActions.toggleDrawer())
+                  }>
+                  <Icon
+                    name="menu"
+                    color="#fff"
+                    size={30}
+                    style={{padding: 10}}
+                  />
+                </TouchableOpacity>
               ),
             })}
-            name="Audio"
-            component={Audio}
           />
-          <Tab.Screen
-            options={(route) => ({
-              tabBarIcon: ({focused, color, size}) => (
-                <Icon name="file-document" size={size} color={color} />
-              ),
-            })}
-            name="Documents"
-            component={Documents}
-          />
-          <Tab.Screen
-            options={(route) => ({
-              tabBarIcon: ({focused, color, size}) => (
-                <Icon name="video" size={size} color={color} />
-              ),
-            })}
-            name="Video"
-            component={Video}
-          />
-          <Tab.Screen
-            options={(route) => ({
-              tabBarIcon: ({focused, color, size}) => (
-                <Icon name="whatsapp" size={size} color={color} />
-              ),
-            })}
-            name="WhatsApp"
-            component={WhatsApp}
-          />
-        </Tab.Navigator>
+        </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
   );
---
+++ app/Injil.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import globalStyles from './styles/globalStyles';
+
+const Injil = () => {
+  return (
+    <ImageBackground
+      style={globalStyles.imgBackground}
+      resizeMode="cover"
+      source={require('./images/background.png')}>
+      <View style={globalStyles.overlay} />
+      <View>
+        <Text />
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Injil;
