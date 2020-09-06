a7674b6c97310dd38a48312c500e316d836f2ebe
Henry Moule <henry@care-planner.co.uk>
2020-09-06 23:51:35 +0100

    fix jesus film not working on android

--- app/constants.ts
+++ app/constants.ts
@@ -12,7 +12,7 @@ export const ROOT_URL = 'https://www.tachelhit.info/';
 export const JESUS_FILM_EMBED_URI =
   'https://api.arclight.org/videoPlayerUrl?refId=1_507-jf-0-0&parentSessionId=5f3efb5825be33.03548638&apiSessionId=5f3f9bd1425a75.29140121&playerStyle=vanilla&player=bc.vanilla6';
 
-export const JESUS_FILM_URI = 'http://tachelhit.info/mp4/JESUS.mp4';
+export const JESUS_FILM_URI = 'https://tachelhit.info/mp4/JESUS.mp4';
 export const MAYLLI_URI =
   'https://www.youtube.com/embed/videoseries?list=PLuAn8llPU4qSMedvpI9jwwEvyFyn-z2jy&autoplay=1&rel=0&autoplay=1';
 
